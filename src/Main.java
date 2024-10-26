c7d4547b327ab4a1437e34606629e213a08d1258
Abhijeet Dhekane <abhijeetdhekane2002@gmail.com>
2024-10-26 23:41:01 +0530

    hheheh

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String url ="jdbc:mysql://localhost:3306/tran";
+        String name ="root";
+        String pass = "abhijeet@2002";
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+        }catch (ClassNotFoundException e){
+            e.getException();
+        }
+        try{
+            Connection con = DriverManager.getConnection(url,name,pass);
+            System.out.println("Connection is done");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
