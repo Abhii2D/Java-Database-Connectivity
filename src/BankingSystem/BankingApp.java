3a35e22da06ec4fcc5a04175dc7052503641b837
Abhijeet Dhekane <abhijeetdhekane2002@gmail.com>
2024-10-28 20:45:02 +0530

    Latest Update

--- src/BankingSystem/BankingApp.java
+++ src/BankingSystem/BankingApp.java
@@ -1,21 +1,16 @@
-import java.sql.*;
-import java.util.Scanner;
+package BankingSystem;
 
-public class Main {
+public class BankingApp {
     public static void main(String[] args) {
-
         String url ="jdbc:mysql://localhost:3306/tran";
         String name ="root";
         String pass = "";
         String que = "insert into emp(name,job,sal) values(?,?,?)";
         try{
             Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("The Drivers are loaded");
         }catch (ClassNotFoundException e){
             e.getException();
         }
-
-
-
-
     }
 }
